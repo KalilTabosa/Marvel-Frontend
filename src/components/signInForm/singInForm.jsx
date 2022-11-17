a14f7611ea983479786a7559d4d90e75ff74d3d4
Kalil Tabosa <kalillegal@gmail.com>
2022-11-17 17:43:14 -0300

    adicionando formik

--- src/components/signInForm/singInForm.jsx
+++ src/components/signInForm/singInForm.jsx
@@ -1,14 +1,39 @@
+import { Link } from "react-router-dom"
+import { useFormik } from "formik"
+
 export const SigninForm = () => {
-    return <div className="m-48">
-        <form className="grid justify-center ">
-        <h4 className="text-center text-2xl">Sign In</h4>
-        <div className="grid gap-3">
-            <label>Email</label>
-            <input type="text" name="email" className="p-2 w-96 h-9 border-black border-2"/>
-            <label>Password</label>
-            <input type="password" name="password" className="p-2 w-96 h-9 border-black border-2"/>
-            <button className="bg-red-700 h-10 rounded text-white">Sign In</button>
+
+    
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        onSubmit: values => {
+            console.log(values);
+        },
+    });
+
+
+    return( 
+        <div className="m-44 grid justify-center">
+            <form onSubmit={formik.handleSubmit}>
+                <div>    
+                    <h4 className="text-center text-2xl">Sign In</h4>
+                    <p className="text-center">don't have a marvel account? <Link to='/signup' className="underline underline-offset-1 text-blue-500">Sign Up</Link></p>
+                </div>
+
+                <div className="grid gap-3">
+                    <label>Email</label>
+                    <input id="Email" type="email" name="email" onChange={formik.handleChange} value={formik.values.email} className="p-2 w-96 h-9 border-black border-2"/>
+
+                    <label>Password</label>
+                    <input id="Password" type="password" name="password" onChange={formik.handleChange} value={formik.values.password} className="p-2 w-96 h-9 border-black border-2"/>
+
+                    <button type="submit" className="bg-red-700 h-10 rounded text-white">Sign In</button>
+                </div>
+            </form>
         </div>
-        </form>
-    </div>
+    )
 }
--- src/components/signUpForm/signUpForm.jsx
+++ src/components/signUpForm/signUpForm.jsx
@@ -1,15 +1,40 @@
+import { useFormik } from 'formik'
+import { Link } from 'react-router-dom';
+
 export const SignupForm = () => {
-    return <div className="m-36">
-        <form className="grid justify-center ">
-        <h4 className="text-center text-2xl">Sign Up</h4>
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            username: '',
+            password: '',
+        },
+        onsubmit: values => {
+            console.log(values);
+        },
+    });
+
+    return <div className="m-28 grid justify-center">
+
+        <form onsubmit={formik.handleSubmit}>
+
+        <div className='m-2'>
+            <h4 className="text-center text-2xl m-2">Sign Up</h4>
+            <p className='text-center'>Already have a Marvel account? <Link to='/signin' className='underline underline-offset-1 text-blue-500 '>Sign In</Link></p>
+        </div>
+
         <div className="grid gap-3">
+
             <label>Email</label>
-            <input type="text" name="email" className="p-2 w-96 h-9 border-black border-2"/>
+            <input id='Email' type="email" name="email" onChange={formik.handleChange} value={formik.values.email} className="p-2 w-96 h-9 border-black border-2"/>
+
             <label>Username</label>
-            <input type="text" name="email" className="p-2 w-96 h-9 border-black border-2"/>
+            <input id='Username' type="text" name="username" onChange={formik.handleChange}  value={formik.values.username} className="p-2 w-96 h-9 border-black border-2"/>
+
             <label>Password</label>
-            <input type="password" name="password" className="p-2 w-96 h-9 border-black border-2"/>
-            <button className="bg-red-700 h-10 rounded text-white">Sign Up</button>
+            <input id='Password' type="password" name="password" onChange={formik.handleChange}  value={formik.values.password} className="p-2 w-96 h-9 border-black border-2"/>
+
+            <button type='submit' className="bg-red-700 h-10 rounded text-white">Sign Up</button>
            
         </div>
         </form>
